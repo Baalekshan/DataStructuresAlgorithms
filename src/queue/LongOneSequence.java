1051e6fc019e049359c0b66abf4b7c70104e1e33
Baalekshan <baalekshan@gmail.com>
2024-09-22 23:07:08 +0530

    added code

--- src/queue/LongOneSequence.java
+++ src/queue/LongOneSequence.java
@@ -1,2 +1,11 @@
-package queue;public class LongOneSequence {
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LongOneSequence {
+    public static void longOneSequence(int[] input, int k) {
+        Queue<Integer> queue = new LinkedList<>();
+        return ;
+    }
 }
